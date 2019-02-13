2f71c5090edb175fb94dd708edba05011af28c91
Pam Olson <pamolson@Pams-MacBook-Air.local>
2019-02-12 20:04:11 -0800

    done

--- interview.java
+++ interview.java
@@ -1,15 +1,22 @@
 //reads a list of numbers from a text file, sorts the numbers, and writes out the sorted list to a
 //file with a “-sorted” suffix and prints to screen the time taken to perform the sort.
 //Implement a 2nd sorting method and produce a comparison of them.
+//Adam Olson
 
 import java.util.*;
 import java.io.*;
 import java.lang.*;
 
-
 public class interview{
-  public static void main(String[]args){
+
+  public static void main(String[]args) throws IOException{
+    int [] array = readInputFile();
+    test(array);
+  }
+
+  public static int [] readInputFile(){
     File file = new File("numbers.txt");
+    //ArrayList<Integer> due to uncertainty regarding size of array
     ArrayList<Integer> list = new ArrayList<Integer>();
     try{
       Scanner scanner = new Scanner(file);
@@ -20,17 +27,42 @@ public class interview{
     }catch (FileNotFoundException e){
       System.out.println("The file " + file.getPath() + " was not found.");
     }
-    //maybe make this a method by itself
+    //ArrayList<Integer> to int[] conversion
     int [] array = new int[list.size()];
     for(int i = 0; i < array.length; i++){
       array[i] = list.get(i).intValue();
     }
-    System.out.println(Arrays.toString(array));
-    System.out.println("Sorted array: " + Arrays.toString(mergeSort(array)));
+  return array;
+  }
+
+  public static void test(int[] array) throws IOException{
+    System.out.println();
+    long mergeStart = System.nanoTime();
+    mergeSort(array);
+    long mergeEnd = System.nanoTime();
+
+    long quickStart = System.nanoTime();
+    quickSort(array);
+    long quickEnd = System.nanoTime();
+
+    long mergeTime = mergeEnd - mergeStart;
+    long quickTime = quickEnd - quickStart;
+
+    //write to output file "output-sorted.txt"
+    PrintWriter out = new PrintWriter("output.txt");
+    out.println("Sorting time results");
+    out.println("MergeSort: " + mergeTime + " nanoseconds.");
+    out.println("Quicksort: " + quickTime + " nanoseconds.");
+    out.println("Testing concluded.");
+    out.close();
+
+    System.out.println("Sorting time results: ");
+    System.out.println("Mergesort: " + mergeTime + " nanoseconds.");
+    System.out.println("Quicksort: " + quickTime + " nanoseconds.");
+    System.out.println("Testing concluded.");
   }
 
   public static int[] mergeSort(int [] unsortedArray){
-
     int [] A = unsortedArray;
     if(A.length > 1){
       int mid = A.length/2;
@@ -71,6 +103,7 @@ public class interview{
       int [] A = recQuickSort(unsortedArray, 0, unsortedArray.length - 1);
       return A;
     }
+
     public static int[] recQuickSort (int [] unsortedArray, int first, int last){
       int [] A = unsortedArray;
       if(first < last){
@@ -80,19 +113,34 @@ public class interview{
       }
     return A;
     }
+
     public static int partition(int [] A, int first, int last){
       int pivot = A[first];
       int r = last;
       int l = first + 1;
       int temp;
       boolean done = false;
-
+      //looking for an 'l' that is greater than pivot
+      //and 'r' that is less than pivot
+      //if r is less than l swap
       while (!done){
-        while(l <= r && A[1] <= pivotvalue){
+        while(l <= r && A[l] <= pivot){
           l = l + 1;
         }
-        
+        while( A[r] >= pivot && r >= l){
+          r = r - 1;
+        }
+        if (r < l){
+          done = true;
+        }else{
+          temp = A[l];
+          A[l] = A[r];
+          A[r] = temp;
+        }
       }
+      temp = A[first];
+      A[first] = A[r];
+      A[r] = temp;
+      return r;
     }
-
 }
